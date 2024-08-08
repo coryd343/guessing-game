d7d6e44cb85d82874b23e04c095f6b88550bbbf0
coryd343 <fizzy.gutsnipe@outlook.com>
2024-08-08 02:11:36 -0700

    Removed function bodies and added comments.

--- main.js
+++ main.js
@@ -1,40 +1,30 @@
+/**
+ * Returns a random integer whose value is less than or equal to the given max value.
+ * @param {number} max 
+ * @returns 
+ */
 function getRandom(max) {
-    return Math.floor(Math.random() * max) + 1;
+    
 }
 
+/**
+ * Returns true if the given guess exists, is a number, is positive and less than the max.
+ * @param {number} guess 
+ * @param {number} max 
+ * @returns 
+ */
 function guessIsValid(guess, max) {
-    return guess !== undefined
-    && !isNaN(guess)
-    && guess > 0
-    && guess <= max;
+
 }
 
+/**
+ * Executes the guessing game. 
+ * Picks a random number to be the hidden "magic" number which the user is prompted to guess.
+ * Prompts the user to guess as long as the number of attempts has not exceeded the number of chances AND as long as the
+ * magic number has not been guessed.
+ * Provides feedback to the user after each guess indicating whether their guess was too low, too high or if it was the correct answer
+ * Displays a "Game Over" message if the user has not guessed correctly before their last attempt.
+ */
 function guessingGame() {
-    const chances = 10;
-    let attempts = 1;
-    let currGuess = -1;
-    const max = 100;
-    const magicNumber = getRandom(max);
-
-    alert(`Guess the number between 1 and ${max} in less than 10 chances!`);
-    while(attempts <= chances) {
-        currGuess = prompt(`What is your guess? (${chances + 1 - attempts} chances remaining)`);
-        if (!guessIsValid(currGuess, max)) {
-            alert(`Please enter a valid number (>0 and <${max}`);
-        }
-        else if (currGuess < magicNumber) {
-            alert("Too low");
-        }
-        else if (currGuess > magicNumber) {
-            alert("Too high");
-        }
-        else {
-            alert(`${magicNumber} was the correct number! You guessed in ${attempts} attempts.`);
-            break;
-        }
-        attempts++;
-    }
-    if (attempts > chances) {
-        alert("Sorry, you are out of chances!");
-    }
+    
 }
